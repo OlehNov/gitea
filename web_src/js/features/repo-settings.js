OlehNov/gitea

commit a32ab0bb8a8edd8776bd97e7643765c38c35ca61

Remove polluted ".ui.left" style (#26809)

templates/repo/settings/collaboration.tmpl
@@ -41,12 +41,8 @@
 		<div class="ui bottom attached segment">
 			<form class="ui form" id="repo-collab-form" action="{{.Link}}" method="post">
 				{{.CsrfTokenHtml}}
-				<div class="inline field ui left">
-					<div id="search-user-box" class="ui search">
-						<div class="ui input">
-							<input class="prompt" name="collaborator" placeholder="{{.locale.Tr "repo.settings.search_user_placeholder"}}" autocomplete="off" autofocus required>
-						</div>
-					</div>
+				<div id="search-user-box" class="ui search input gt-vm">
+					<input class="prompt" name="collaborator" placeholder="{{.locale.Tr "repo.settings.search_user_placeholder"}}" autocomplete="off" autofocus required>
 				</div>
 				<button class="ui green button">{{.locale.Tr "repo.settings.add_collaborator"}}</button>
 			</form>
@@ -93,12 +89,8 @@
 			{{if $allowedToChangeTeams}}
 				<form class="ui form" id="repo-collab-team-form" action="{{.Link}}/team" method="post">
 					{{.CsrfTokenHtml}}
-					<div class="inline field ui left">
-						<div id="search-team-box" class="ui search" data-org="{{.OrgName}}">
-							<div class="ui input">
-								<input class="prompt" name="team" placeholder="{{$.locale.Tr "repo.settings.search_team"}}" autocomplete="off" autofocus required>
-							</div>
-						</div>
+					<div id="search-team-box" class="ui search input gt-vm">
+						<input class="prompt" name="team" placeholder="{{$.locale.Tr "repo.settings.search_team"}}" autocomplete="off" autofocus required>
 					</div>
 					<button class="ui green button">{{$.locale.Tr "repo.settings.add_team"}}</button>
 				</form>

web_src/css/repo.css
@@ -1907,24 +1907,6 @@
   flex-wrap: wrap;
 }
 
-.repository.settings.collaboration #repo-collab-form #search-user-box .results {
-  left: 7px;
-}
-
-.repository.settings.collaboration #repo-collab-form .ui.button {
-  margin-left: 5px;
-  margin-top: -3px;
-}
-
-.repository.settings.collaboration #repo-collab-team-form #search-team-box .results {
-  left: 7px;
-}
-
-.repository.settings.collaboration #repo-collab-team-form .ui.button {
-  margin-left: 5px;
-  margin-top: -3px;
-}
-
 /* if the element is for a checkbox, then it should have a padding-left to align to the checkbox's text */
 .repository.settings.branches .branch-protection .ui.checkbox .help,
 .repository.settings.branches .branch-protection .checkbox-sub-item {

web_src/css/base.css
@@ -1179,11 +1179,6 @@ img.ui.avatar,
   font-size: 0.75em;
 }
 
-/* FIXME: this is a serious pollution, do not use this for "float: left" anymore */
-.ui.left:not(.action) {
-  float: left;
-}
-
 /* FIXME: this is a serious pollution, do not use this for "float: right" anymore */
 .ui.right:not(.action) {
   float: right;

web_src/js/components/DashboardRepoList.vue
@@ -15,7 +15,7 @@
         </a>
       </h4>
       <div class="ui attached segment repos-search">
-        <div class="ui fluid right action left icon input" :class="{loading: isLoading}">
+        <div class="ui fluid action left icon input" :class="{loading: isLoading}">
           <input type="search" spellcheck="false" maxlength="255" @input="changeReposFilter(reposFilter)" v-model="searchQuery" ref="search" @keydown="reposFilterKeyControl" :placeholder="textSearchRepos">
           <i class="icon"><svg-icon name="octicon-search" :size="16"/></i>
           <div class="ui dropdown icon button" :title="textFilter">

commit 4365dbc1375ed4827b7792319b681bad759a0653

Improve repo/user/org search  (#27030)
* Fix a regression from #26809 (the `data-org` is missing)
* Remove unnecessary style

Screenshots:


![image](https://github.com/go-gitea/gitea/assets/2114189/3f5cf628-db7f-4705-898a-7a4a1fbfbba8)


![image](https://github.com/go-gitea/gitea/assets/2114189/453d1fad-1090-4524-bf45-6c5da2465f04)


![image](https://github.com/go-gitea/gitea/assets/2114189/f14d9808-7596-42c8-84b4-0d57a0bf2278)

templates/repo/settings/collaboration.tmpl
@@ -89,7 +89,7 @@
 			{{if $allowedToChangeTeams}}
 				<form class="ui form" id="repo-collab-team-form" action="{{.Link}}/team" method="post">
 					{{.CsrfTokenHtml}}
-					<div id="search-team-box" class="ui search input gt-vm">
+					<div id="search-team-box" class="ui search input gt-vm" data-org-name="{{.OrgName}}">
 						<input class="prompt" name="team" placeholder="{{$.locale.Tr "repo.settings.search_team"}}" autocomplete="off" autofocus required>
 					</div>
 					<button class="ui green button">{{$.locale.Tr "repo.settings.add_team"}}</button>

web_src/css/repo.css
@@ -2154,21 +2154,15 @@
   margin-top: 5px;
 }
 
-#search-repo-box .results .result img,
-#search-user-box .results .result img {
+#search-user-box .results .result .image {
   float: left;
   margin-right: 8px;
   width: 2em;
   height: 2em;
 }
 
-#search-repo-box .results .result .content,
 #search-user-box .results .result .content {
-  margin: 6px 0;
-}
-
-#search-team-box .results .result .content {
-  margin: 6px 0;
+  margin: 6px 0; /* this trick is used to align with the sibling avatar image */
 }
 
 .ui.menu .item > img:not(.ui) {

web_src/js/features/repo-settings.js
@@ -47,7 +47,7 @@ export function initRepoSettingSearchTeamBox() {
   $searchTeamBox.search({
     minCharacters: 2,
     apiSettings: {
-      url: `${appSubUrl}/org/${$searchTeamBox.data('org')}/teams/-/search?q={query}`,
+      url: `${appSubUrl}/org/${$searchTeamBox.attr('data-org-name')}/teams/-/search?q={query}`,
       headers: {'X-Csrf-Token': csrfToken},
       onResponse(response) {
         const items = [];